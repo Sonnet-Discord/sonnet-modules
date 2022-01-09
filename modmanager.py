053a7cfc9387b2e14fca534a5d5e00bbc18b5e10
ultrabear <bearodark@gmail.com>
2022-01-08 23:06:26 -0800

    add .modules.info to modmanager

--- modmanager.py
+++ modmanager.py
@@ -6,8 +6,9 @@ import sys
 import os
 import argparse
 import shutil
+import json
 
-from typing import Optional, List, Tuple, Generator
+from typing import Optional, List, Tuple, Generator, Type, Dict, Any
 
 
 class UserError(Exception):
@@ -54,6 +55,85 @@ if SONNET_DIR is None or SONNET_MOD_DIR is None:
     raise RuntimeError("Could not grab SONNET_PATH or SONNET_MOD_PATH (You may need to add paths to env vars)")
 
 
+class BJsonConfig:
+    __slots__ = "commands", "tables", "configs", "events", "caches", "files", "version"
+    def __init__(self, data: Dict[str, Any]) -> None:
+        self.commands: List[str] = data.get("commands", [])
+        self.tables: List[str] = data.get("tables", [])
+        self.configs: List[str] = data.get("configs", [])
+        self.events: List[str] = data.get("events", [])
+        self.caches: List[str] = data.get("caches", [])
+        self.files: List[str] = data.get("files", [])
+        self.version: str = data.get("version", "0.0.0")
+
+class ModuleEntry:
+    __slots__ = "author", "name", "version"
+    def __init__(self, author: str, name: str, version: str) -> None:
+        self.author = author
+        self.name = name
+        self.version = version
+
+def bloat_json_to_obj(filename: str) -> BJsonConfig:
+    with open(filename, "rb") as fp:
+        return BJsonConfig(json.load(fp))
+
+
+class ModuleDB:
+    """ModuleDB stores the state of modules in a file for version control"""
+    __slots__ = "_fp", "_modules"
+
+    def __init__(self) -> None:
+        try:
+            self._fp = open(f"{SONNET_DIR}.modules.info", "r+", encoding="utf8")
+        except FileNotFoundError:
+            self._fp = open(f"{SONNET_DIR}.modules.info", "w+", encoding="utf8")
+
+        preparsed = self._fp.read().split("\n")
+        self._modules: List[ModuleEntry] = []
+
+        for i in filter(lambda s: s, preparsed):
+            author, name, version = i.split("/")
+            self._modules.append(ModuleEntry(author, name, version))
+
+    def __enter__(self) -> "ModuleDB":
+        return self
+
+    def install(self, author: str, modname: str) -> BJsonConfig:
+        info = bloat_json_to_obj(f"{SONNET_MOD_DIR}mod/{author}/{modname}/bloat.json")
+
+        if info.version == "0.0.0":
+            print(f"Warning: Module {author}/{modname}: has no version specified, it will not be auto updated")
+
+        self._modules.append(ModuleEntry(author, modname, info.version))
+
+        return info
+
+
+    def uninstall(self, author: str, modname: str) -> None:
+        delidx: Optional[int] = None
+
+        for idx, i in enumerate(self._modules):
+            if i.author == author and i.name == modname:
+                delidx = idx
+
+        if delidx is not None:
+            del self._modules[delidx]
+        else:
+            print(f"Warning: {author}/{modname}: This module is not in the install file")
+
+    def save(self) -> None:
+        self._fp.seek(0)
+        for i in self._modules:
+            self._fp.write(f"{i.author}/{i.name}/{i.version}\n")
+        self._fp.truncate()
+
+    def __exit__(self, e_type: Optional[Type[Exception]], ex: Optional[Exception], tb: Any) -> None:
+        self.save()
+        self._fp.close()
+        if e_type:
+            raise e_type(ex)
+
+
 def get_module_filenames(user: str, modulename: str) -> List[str]:
 
     moduledir = os.listdir(f"{SONNET_MOD_DIR}mod/{user}/{modulename}")
@@ -85,6 +165,8 @@ def install_module(user: str, modulename: str) -> None:
         print(f"Installing {i} to {SONNET_DIR}{folder}/{i}")
         shutil.copy(f"{SONNET_MOD_DIR}mod/{user}/{modulename}/{i}", f"{SONNET_DIR}{folder}/{i}")
 
+    with ModuleDB() as db:
+        db.install(user, modulename)
 
 def uninstall_module(user: str, modulename: str) -> None:
     print(f"Uninstalling {user}/{modulename}")
@@ -99,6 +181,8 @@ def uninstall_module(user: str, modulename: str) -> None:
         print(f"Deleting {SONNET_DIR}{folder}/{fname}")
         os.remove(f"{SONNET_DIR}{folder}/{fname}")
 
+    with ModuleDB() as db:
+        db.uninstall(user, modulename)
 
 def validate_modulename(modname: str) -> Tuple[str, str]:
 
@@ -121,6 +205,8 @@ def main(args: List[str]) -> int:
 
     parser.add_argument("-S", help="Install the module to the sonnet install directory", dest="install")
     parser.add_argument("-R", help="Uninstall a module from the sonnet install directory", dest="uninstall")
+    #TODO(ultrabear): add this
+    #parser.add_argument("-U", action="store_true", help="Update all installed packages", dest="update")
 
     parsed = parser.parse_args()
 
